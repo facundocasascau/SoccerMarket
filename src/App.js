bb4f72b6b0a15bd76533f9e19f36acafb92d5763
Facundo Casas Cau <facundocasascau@gmail.com>
2020-10-02 02:02:38 -0300

    empezando item detail

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 import Nav from './components/NavBar';
 import './components/styles.css'; 
 import ItemCount from './components/ItemCount';
+import Item from './components/Item'
+import Loading from './components/Loading'
+import ItemList from './components/ItemList';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <ItemCount/>
+      <ItemList/>
     </div>
   );
 }
